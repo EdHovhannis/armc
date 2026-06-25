2656804c6400b651aeca99dad272a57f138b1808
Ed Hovhannisyan <ed.hovhannis1992@gmail.com>
2026-06-25 08:29:54 +0300

    add cnopka

--- src/pages/ArchivesEditPage/Steps/Footer.tsx
+++ src/pages/ArchivesEditPage/Steps/Footer.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@sds-eng/base';
 import { useUnit } from 'effector-react';
 import { FC } from 'react';
+import { useFormContext, useWatch } from 'react-hook-form';
 
 import { $stepperIndex, onChangeStepperIndex } from '@src/Widgets/ArchiveEditStepper/model';
 
@@ -8,12 +9,18 @@ import * as styles from './styles.module.css';
 
 const ArchiveEditFooter: FC = () => {
   const [stepperIndex, onChangeStepperIndexFn] = useUnit([$stepperIndex, onChangeStepperIndex]);
+  const { control } = useFormContext();
+  const [name, project] = useWatch({ control, name: ['name', 'project'] });
+
+  const isIndexNameStepValid = Boolean(String(name ?? '').trim()) && Boolean(project);
+  const isNextDisabled = stepperIndex === 5 || (stepperIndex === 0 && !isIndexNameStepValid);
+
   return (
     <div className={styles.archiveFooterWrapper}>
       <Button size="md" view="secondary" disabled={stepperIndex === 0} onClick={() => onChangeStepperIndexFn(stepperIndex - 1)}>
         Назад
       </Button>
-      <Button size="md" view="primary" disabled={stepperIndex === 5} onClick={() => onChangeStepperIndexFn(stepperIndex + 1)}>
+      <Button size="md" view="primary" disabled={isNextDisabled} onClick={() => onChangeStepperIndexFn(stepperIndex + 1)}>
         Далее
       </Button>
     </div>
